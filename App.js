1ca5c7f39bd1950bb8107b8459195c26359dd7a3
Priyansh Kumar <priyansh803213@gmail.com>
2025-07-20 00:30:03 +0530

    Episode-02

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello World from React!"
-);
+import React from "react";
+import ReactDOM from "react-dom/client"; // Importing React and ReactDOM libraries
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am Priyansh Kumar"),
+    React.createElement("h2", {}, "I am an h2 tag in child2"),
+  ]),
+]);
+
+console.log(parent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
